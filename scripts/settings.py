48df4fdb471959ec25d42f5af09322bb0707f148
J vanBemmel <jvb127@gmail.com>
2021-10-28 18:52:38 -0500

    Create settings.py

---
+++ scripts/settings.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+REPO_URL = os.getenv("REPO_URL")
+NETBOX_URL = os.getenv("NETBOX_URL")
+NETBOX_TOKEN = os.getenv("NETBOX_TOKEN")
+IGNORE_SSL_ERRORS = (os.getenv("IGNORE_SSL_ERRORS", "False") == "True")
+
+# optionnally load vendors through a space separated list as env var
+VENDORS = os.getenv("VENDORS", "").split()
+
+MANDATORY_ENV_VARS = ["REPO_URL", "NETBOX_URL", "NETBOX_TOKEN"]
+
+for var in MANDATORY_ENV_VARS:
+    if var not in os.environ:
+        raise EnvironmentError("Failed because {} is not set.".format(var))
