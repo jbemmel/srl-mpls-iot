ab5b0aae1e50a42db59b2725428d0e95e1bdf1e4
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-11-02 12:48:24 -0500

    Add os.environ

--- pysros-demo.py
+++ pysros-demo.py
@@ -1,11 +1,13 @@
 # Map this file to /tftpboot/ for vrnet vr-sros VMs to access via tftp
-import sys
+import sys, os
 from pysros.management import connect
 
 
 c = connect()
 pysros_ds = c.running.get("/nokia-conf:configure/system/name")
-print( pysros_ds )
+print( f"/nokia-conf:configure/system/name: {pysros_ds}" )
+
+print( f"os.environ: {os.environ}" )
 
 # Be a good netizen
 sys.exit( 0 )
