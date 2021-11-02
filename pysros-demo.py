4e776fe4122e020488d21ff1664a25573bafaad3
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-11-02 13:05:32 -0500

    No 'os' module on SROS

--- pysros-demo.py
+++ pysros-demo.py
@@ -1,13 +1,15 @@
 # Map this file to /tftpboot/ for vrnet vr-sros VMs to access via tftp
-import sys, os
+import sys
+# import os  # does not work - "no module named 'os'"
 from pysros.management import connect
 
 
 c = connect()
 pysros_ds = c.running.get("/nokia-conf:configure/system/name")
-print( "/nokia-conf:configure/system/name: {}".format( pysros_ds ) )
+print( "/nokia-conf:configure/system/name: %s" % pysros_ds )
 
-print( "os.environ: {}".format( os.environ ) )
+print( "sys.version: %s" % sys.version )
+# print( "os.environ: {}".format( os.environ ) )
 
 # Be a good netizen
 sys.exit( 0 )
