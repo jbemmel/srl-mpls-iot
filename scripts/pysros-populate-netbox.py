9eb243acae43fe18042ed8bb60c74aeec26c65e7
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-11-05 22:36:03 -0500

    pySROS

---
+++ scripts/pysros-populate-netbox.py
@@ -0,0 +1,40 @@
+# Map this file to /tftpboot/ for vrnet vr-sros VMs to access via tftp
+import sys
+# import os  # does not work - "no module named 'os'"
+from pysros.management import connect
+import pynetbox
+
+c = connect()
+
+#
+# Uses Netconf to retrieve SROS YANG model config and/or system state
+#
+platform = c.running.get("/nokia-state:state/system/platform")
+print( "/nokia-state:state/system/platform: %s" % platform )
+
+# "configure": {
+#             "nokia-conf:card": [
+#               {
+#                 "fp": [
+#                   {
+#                     "fp-number": 1
+#                   },
+#                   {
+#                     "fp-number": 2
+#                   }
+#                 ],
+#                 "mda": [
+#                   {
+#                     "mda-slot": 1,
+#                     "mda-type": "s36-100gb-qsfp28"
+
+# Read MDA type(s)
+mda_type = c.running.get("/nokia-state:state/card/mda/equipped-type")
+print( "/nokia-state:state/card/mda/equipped-type: %s" % mda_type )
+
+#
+# Create or update device in Netbox
+#
+
+# Be a good netizen
+sys.exit( 0 )
