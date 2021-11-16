5ebea5385a4063f5b3ff4659b91f2927f4d58258
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-11-16 12:35:47 -0600

    str

--- scripts/pysros-populate-netbox.py
+++ scripts/pysros-populate-netbox.py
@@ -68,8 +68,8 @@ def createDeviceType(deviceTypeName, cards, nb):
     for c in cards:
        for m in cards[c]['mda']:
           portCount = int( str( cards[c]['mda'][m]['equipped-ports']) )
-          mdaType = cards[c]['mda'][m]['equipped-type']
-          portType = '100gbase-x-qsfp28' if ('qsfp' in mdaType) else '400gbase-x-qsfpdd'
+          mdaType = str( cards[c]['mda'][m]['equipped-type'] )
+          portType = '100gbase-x-qsfp28' if 'qsfp' in mdaType else '400gbase-x-qsfpdd'
           print( "Card %d MDA %d: ports %d type=%s => %s" % (c,m,portCount,mdaType,portType) )
           createInterfaces(c, m, portCount, portType, dev_type.id, nb)
 
