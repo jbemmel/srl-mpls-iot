1958a87b44eb69d5fa02cdb40f662808612104d5
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-11-30 23:43:20 -0600

    Extend sample topology

---
+++ labs/sr-isis-evpn/delta-auto-config.cmd
@@ -0,0 +1,4 @@
+set /system gnmi-server unix-socket admin-state enable
+set /auto-config-agent gateway ipv4 10.0.0.1/24
+set /auto-config-agent lacp active # reload-delay-secs 0
+set /auto-config-agent igp isis evpn model symmetric-irb auto-lags encoded-ipv6 overlay-as 65000 route-reflector spine
